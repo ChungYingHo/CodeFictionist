d1bfc9da805489c4476a38df764b1576d1fc36aa
Chung-Ying <ag.cyho@gmail.com>
2023-12-23 19:36:41 +0800

    add article

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CodeFictionist',
-  tagline: 'Dinosaurs are cool',
+  tagline: '寫程式的小說家',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
